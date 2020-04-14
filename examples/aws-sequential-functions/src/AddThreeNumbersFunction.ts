8b717a82bffa4ec20c2d199824055c73fe42f2bf
Andy Blackledge <andyblackledge@hotmail.com>
2020-04-14 21:49:18 +0100

    Got flow being event-driven

--- examples/aws-sequential-functions/src/AddThreeNumbersFunction.ts
+++ examples/aws-sequential-functions/src/AddThreeNumbersFunction.ts
@@ -1,19 +1,26 @@
-import { AddThreeNumbersRequest, AddThreeNumbersResponse } from './exchanges/AddThreeNumbersExchange';
+import { SNSEvent } from 'aws-lambda';
+import { AddThreeNumbersRequest } from './exchanges/AddThreeNumbersExchange';
 import { FlowContext } from './omahdog/FlowContext';
 import { FlowHandlers } from './omahdog/FlowHandlers';
 import { SumNumbersRequest, SumNumbersResponse } from './exchanges/SumNumbersExchange';
 import { SumNumbersHandler } from './handlers/SumNumbersHandler';
 import { AddThreeNumbersHandler } from './handlers/AddThreeNumbersHandler';
+import { SNSFlowMessage } from './omahdog-aws';
 
 const handlers = new FlowHandlers()
     .register(SumNumbersRequest, SumNumbersResponse, new SumNumbersHandler());
     
-export const handler = async (event: AddThreeNumbersRequest): Promise<AddThreeNumbersResponse | undefined> => {
+export const handler = async (event: SNSEvent): Promise<void> => {
+
+    const snsMessage = event.Records[0].Sns;    
+    // TODO 14Apr20: Check snsMessage.MessageAttributes
+    const message = JSON.parse(snsMessage.Message) as SNSFlowMessage;    
+    const request = message.body as AddThreeNumbersRequest;
 
     const flowContext = new FlowContext();
     flowContext.handlers = handlers;
 
-    const response = new AddThreeNumbersHandler().handle(flowContext, event);
-    
-    return response;
+    const response = new AddThreeNumbersHandler().handle(flowContext, request);
+
+    console.log(`response: ${JSON.stringify(response)}`);
 };
--- examples/aws-sequential-functions/test/AddThreeNumbersFunction.tests.ts
+++ examples/aws-sequential-functions/test/AddThreeNumbersFunction.tests.ts
@@ -1,17 +1,58 @@
-import { expect } from 'chai';
 import { handler } from '../src/AddThreeNumbersFunction';
 import { AddThreeNumbersRequest } from '../src/exchanges/AddThreeNumbersExchange';
+import { SNSEvent } from 'aws-lambda';
+import { SNSFlowMessage } from '../src/omahdog-aws';
 
-describe('Adder tests', () => {
+describe('handler tests', () => {
 
     it('adds up numbers', async () => {
 
-        const event: AddThreeNumbersRequest = {
+        const request: AddThreeNumbersRequest = {
             a: 202, b: 202, c: 212
         };
-        
-        const response = await handler(event);
 
-        expect(response?.total).to.equal(616);
+        const snsFlowMessage: SNSFlowMessage = {
+            body: request
+        };
+
+        const snsFlowMessageJson = JSON.stringify(snsFlowMessage);
+
+        console.log(snsFlowMessageJson);
+
+        const snsEvent: SNSEvent = {
+            'Records': [
+                {
+                    'EventSource': 'aws:sns',
+                    'EventVersion': '1.0',
+                    'EventSubscriptionArn': 'arn:aws:sns:eu-west-2:{{{accountId}}}:ExampleTopic',
+                    'Sns': {
+                        'Type': 'Notification',
+                        'MessageId': '95df01b4-ee98-5cb9-9903-4c221d41eb5e',
+                        'TopicArn': 'arn:aws:sns:eu-west-2:123456789012:ExampleTopic',
+                        'Subject': 'example subject',
+                        'Message': snsFlowMessageJson,
+                        'Timestamp': '1970-01-01T00:00:00.000Z',
+                        'SignatureVersion': '1',
+                        'Signature': 'EXAMPLE',
+                        'SigningCertUrl': 'EXAMPLE',
+                        'UnsubscribeUrl': 'EXAMPLE',
+                        'MessageAttributes': {
+                            'Test': {
+                                'Type': 'String',
+                                'Value': 'TestString'
+                            },
+                            'TestBinary': {
+                                'Type': 'Binary',
+                                'Value': 'TestBinary'
+                            }
+                        }
+                    }
+                }
+            ]
+        };
+        
+        console.log(JSON.stringify(snsEvent));
+
+        await handler(snsEvent);
     });
 });
